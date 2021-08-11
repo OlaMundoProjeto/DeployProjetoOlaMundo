8c00171133dda52735fd1920c8b5f204ba2e45cb
Pamella Santos <pamellaolys@gmail.com>
2021-08-11 16:24:14 -0300

    Implementação de alertas e pesquisa

---
+++ src/app/model/Postagem.ts
@@ -0,0 +1,15 @@
+import { Tema } from "./Tema"
+import { User } from "./User"
+
+
+export class Postagem {
+  public id: number
+  public titulo: String
+  public texto: String
+  public agenda: String
+  public contato: String
+  public tema: Tema
+  public usuario: User
+  public data: Date
+
+}
